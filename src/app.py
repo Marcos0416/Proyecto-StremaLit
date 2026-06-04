573b35eb991a97a3a80f08ef0140439294aa53d2
Marcos <mtg200416@gmail.com>
2026-06-04 13:33:19 +0200

    subiendo el proyecto de flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,42 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, render_template, request
+import joblib
 
-# your code here
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    values = [
+        float(request.form["pregnancies"]),
+        float(request.form["glucose"]),
+        float(request.form["bloodpressure"]),
+        float(request.form["skinthickness"]),
+        float(request.form["insulin"]),
+        float(request.form["bmi"]),
+        float(request.form["pedigree"]),
+        float(request.form["age"])
+    ]
+
+    prediction = model.predict([values])
+
+    result = (
+        "Posible diabetes"
+        if prediction[0] == 1
+        else "No presenta diabetes"
+    )
+
+    return render_template(
+        "index.html",
+        prediction=result
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
